aae2d72c747c1ab7002fda1412f10f7eafc24e4f
Matheus <matheusdeamorim20@gmail.com>
2023-06-20 16:06:52 -0300

    changing page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,18 @@
 'use client'
+import Link from "next/link"
+import { useRouter } from "next/navigation"
 export default function Home() {
   
   return (
     // Header a ser inserido 
     <div className="w-screen h-screen flex justify-center items-center">
       <div>
-        <h1> Digite /cripto/(a moeda desejada) na URL</h1>
-        <div className="flex justify-center mt-4">
-          <ul>
-            <li>ethereum</li>
-            <li>matic-network</li>
-            <li>binancecoin</li>
-            <li>tether</li>
-          </ul>
+        <h1> Clique na moeda que deseja visualizar</h1>
+        <div className="flex flex-col justify-center items-center mt-4">
+            <Link href="/cripto/ethereum">ethereum</Link>
+            <Link href="/cripto/matic-network">matic</Link>
+            <Link href="/cripto/binancecoin">bnb</Link>
+            <Link href="/cripto/tether">tether</Link>
         </div>
       </div>
 
